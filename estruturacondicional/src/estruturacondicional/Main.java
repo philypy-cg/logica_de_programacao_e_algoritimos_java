d453714b6bf85569afc4e5947d503d522aa76f23
Philypy Gomes <phcg.dev@gmail.com>
2023-03-28 00:07:53 -0300

    exerifelse

--- estruturacondicional/src/estruturacondicional/Main.java
+++ estruturacondicional/src/estruturacondicional/Main.java
@@ -1,9 +1,28 @@
 package estruturacondicional;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Bom dia");
+
+		Scanner sc = new Scanner(System.in);
+		int hora;
+
+		System.out.println("Quantas horas ?");
+		hora = sc.nextInt();
+
+		if (hora < 12) {
+			System.out.println("Bom dia");
+		} 
+		else if (hora < 18) {
+			System.out.println("Boa tarde");
+		} 
+		else {
+			System.out.println("Boa noite");
+		}
+
+		sc.close();
 
 	}
 
---
+++ exerciciocondicional/src/exerciciocondicional/Main.java
@@ -0,0 +1,26 @@
+package exerciciocondicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double nota1 = sc.nextDouble();
+		double nota2 = sc.nextDouble();
+		
+		double notaFinal = nota1 + nota2;
+		System.out.printf("Nota final= %.1f%n", notaFinal);
+		
+		if(notaFinal < 60) {
+			System.out.println("REPROVADO");
+		}
+		
+		
+		sc.close();
+	}
+
+}
